90d0be6ad9af8bf899a3ca4c1d7883a55620913c
Lucas-Angelo <lcs2001_@hotmail.com>
2020-05-15 19:59:33 -0300

    ex08 lab09

---
+++ C/Labs/Lab09/exercicio08.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int funcao(char string[250], char caractere, int i);
+
+int main()
+{
+    char string[250], caractere;
+    printf("Digite uma frase: ");
+    gets(string);
+    printf("Digite um caractere: ");
+    scanf("%c", &caractere);
+    if(funcao(string, caractere, 0)==1)
+    {
+        printf("Existe o caractere %c na frase %s", caractere, string);
+    }
+    else
+    {
+        printf("Nao existe o caractere %c na frase %s", caractere, string);
+    }
+    return 0;
+}
+int funcao(char string[250], char caractere, int i)
+{
+    if(string[i]==caractere)
+    {
+        return 1;
+    }
+    else
+    {
+        return funcao(string,caractere,i+1);
+    }
+}
